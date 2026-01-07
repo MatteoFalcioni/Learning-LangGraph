9855081e9d58b2c06a49e4930dca9ade60c4a343
MatteoFalcioni <matteo.falk@gmail.com>
2026-01-07 11:10:49 +0100

    review status

--- projects/vision_agents/src/graph/state.py
+++ projects/vision_agents/src/graph/state.py
@@ -6,7 +6,7 @@ class MyState(AgentState):
     """
     State for the graph.
     """
-    bookmarked_articles: Annotated[list, add] 
-    parsed_papers: Annotated[list, add]     # list of paper parsed by mistral
+    bookmarked_articles: Annotated[list, add] # list of dictionaries, each containing 'title' and 'id' keys
+    downloaded_papers_paths: Annotated[list, add] # list of strings, each containing the path to the downloaded paper
     generated_images: Annotated[list, add]  # list of generated images
     review_status: str     # list of review status from image reviewer (uses default str replace)
