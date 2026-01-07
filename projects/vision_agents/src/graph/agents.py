17eb6395b39a7ddaa834a87843d6430a85084c6f
MatteoFalcioni <matteo.falk@gmail.com>
2026-01-07 11:27:27 +0100

    structured output for summarizer

--- projects/vision_agents/src/graph/agents.py
+++ projects/vision_agents/src/graph/agents.py
@@ -8,11 +8,9 @@ from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 
 from prompts.arxiv_prompt import arxiv_prompt
-from prompts.nanobanana_prompt import nanobanana_prompt
 from prompts.summarizer import summarizer_prompt
 from prompts.image_reviewer import reviewer_prompt
 from tools.arxiv_tools import search_arxiv, mark_as_relevant, download_pdf, list_downloads, read_by_page, list_marked_articles
-from tools.shared import read_downloaded_paper
 from tools.summarization import produce_summary
 from state import MyState
 
@@ -37,7 +35,6 @@ def get_openrouter_model(model_name: str):
 
 def create_arxiv_agent():
     """ Creates the arxiv agent. """    
-    # ======= ARXIV =======
     arxiv_llm = get_openrouter_model("google/gemini-3-flash-preview")
 
     arxiv_agent = create_agent(
@@ -59,21 +56,27 @@ def create_arxiv_agent():
 
     return arxiv_agent
 
+class SummarizerResponseSchema(BaseModel):
+        """
+        Schema for the summarizer agent response.   
+        Access it as result["structured_response"]
+        """
+        summary: str = Field(description="The summary of the PDF")
+
 def create_summarizer_agent():
     """ Creates the summarizer agent. """    
-    # ======= SUMMARIZER AGENT =======
     summarizer_llm = get_openrouter_model("google/gemini-3-flash-preview")
 
     summarizer_agent = create_agent(
         model=summarizer_llm,
-        tools=[produce_summary],
         system_prompt=summarizer_prompt,
-        state_schema=MyState
+        state_schema=MyState,
+        response_format=SummarizerResponseSchema
     )
 
     return summarizer_agent
 
-class ResponseSchema(BaseModel):
+class ReviewerResponseSchema(BaseModel):
         """
         Schema for the image reviewer agent response.
         Access it as result["structured_response"]
@@ -83,14 +86,13 @@ class ResponseSchema(BaseModel):
 
 def create_image_reviewer_agent():
     """ Creates the reviewer agent: it is a structured output agent"""    
-    # ======= VISION AGENT =======
     image_reviewer = get_openrouter_model("google/gemini-3-flash-preview")
 
     image_reviewer_agent = create_agent(
         model=image_reviewer,
         system_prompt=reviewer_prompt,
         state_schema=MyState,
-        response_format=ResponseSchema
+        response_format=ReviewerResponseSchema
     )
 
     return image_reviewer_agent
